bd2c2cb13d0aea68643d002df04439d630e02847
chenxiaoming <chenxiaomingLS@jomoo.cn>
2022-08-23 18:10:25 +0800

    【fix】:1.删除不必要的代码。

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { createApp, App as myApp } from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -6,7 +6,7 @@ import 'amfe-flexible' // 自适应各种分辨率屏幕
 import installVantUI from './libs/vant'
 import './router/permission'
 import '@/style/index.scss'
-const app = createApp(App) as unknown as myApp
+const app = createApp(App)
 app.use(store)
 app.use(router)
 // 按需加载vant组件
