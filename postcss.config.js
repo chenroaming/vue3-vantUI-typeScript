6c98611d627294aca2edf5545a764ea14debf2c5
chenxiaoming <chenxiaomingLS@jomoo.cn>
2022-08-29 11:28:53 +0800

    【perf】:1.增加注释。

--- postcss.config.js
+++ postcss.config.js
@@ -3,11 +3,13 @@ module.exports = {
   plugins: {
     // postcss-pxtorem 插件的版本需要 >= 5.0.0
     'postcss-pxtorem': {
+      // 如果是vant类型的文件，则以37.5作为基准值，否则设为75
       rootValue ({ file }) {
         return file.includes('vant') ? 37.5 : 75
       },
+      // 代表转换所有css属性
       propList: ['*'],
-      selectorBlackList: ['van']
+      selectorBlackList: ['van-']
     }
   }
 }
--- src/libs/vant.ts
+++ src/libs/vant.ts
@@ -1,9 +1,19 @@
+/*
+ * @Description: 按需引入vant组件，减少编译打包后的体积
+ * @Author: chenroaming
+ * @Date: 2022-07-20 13:48:27
+ * @LastEditors: chenroaming
+ * @LastEditTime: 2022-08-29 09:49:41
+ */
+
 import type { App } from 'vue'
 // 手动引入函数式组件样式，参见：https://vant-contrib.gitee.io/vant/#/zh-CN/quickstart
 import 'vant/es/dialog/style'
 import 'vant/es/toast/style'
 import 'vant/es/notify/style'
 import 'vant/es/image-preview/style'
+// 提示：在单个项目中不应该同时使用「全量引入」和「按需引入」，否则会导致代码重复、样式错乱等问题。
+// 参见：https://vant-contrib.gitee.io/vant/#/zh-CN/quickstart
 import {
   Button,
   Popup,
